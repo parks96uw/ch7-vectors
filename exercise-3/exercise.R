2a2b0267334340397466d11b4a28f510c236fd4a
Sarah Park <parks96@uw.edu>
2017-10-20 22:43:06 -0700

    completed all questions of exercise-3

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -15,9 +15,9 @@ MarbleGame <- function(guess) {
   roll <- sample(marbles, 1)
   win <- guess == roll
   if(win == TRUE) {
-    cat("Correct guess:", guess)
+    cat("You won!")
   } else {
-    cat("Wrong guess:", guess)
+    cat("You lost!")
   }
 }
 
@@ -25,11 +25,32 @@ MarbleGame <- function(guess) {
 MarbleGame("green")
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
-
+has_won <- FALSE
+tries <- 0
+while(!has_won) {
+  roll <- sample(marbles, 1)
+  if (roll == "green") {
+    has_won <- TRUE
+  } else {
+    tries <- tries + 1
+  }
+}
 
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of
 ## tries
-
+total_tries <- 0
+total_wins <- 0
+for (i in 1:1000) {
+  roll <- sample(marbles, 1)
+  if (roll == "green") {
+    total_wins <- total_wins + 1
+  } else {
+    total_tries <- total_tries + 1
+  }
+}
+total_tries
+total_wins
+avg_wins <- total_wins / total_tries
 
 ## Is it what you expected based on the probability?
-
+# Yes, probably of winning a game is about 18 - 22%, almost 1/6
